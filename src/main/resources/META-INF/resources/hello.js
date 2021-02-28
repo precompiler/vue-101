e6f6ae92e6acb3ae10ecc027b98d7f301152ac50
precompiler <lishengyin.msn@hotmail.com>
2021-02-28 20:44:53 +0000

    add ramdom user demo

--- src/main/resources/META-INF/resources/hello.js
+++ src/main/resources/META-INF/resources/hello.js
@@ -7,3 +7,32 @@ const app = Vue.createApp({
 })
 
 app.mount('#hello')
+
+
+const profile = Vue.createApp({
+    data() {
+        return {
+            firstName: '',
+            lastName: '',
+            email: '',
+            gender: '',
+            street: '',
+            picture: ''
+        }
+    },
+    methods: {
+        async nextUser() {
+            const res = await fetch('https://randomuser.me/api')
+            const {results} = await res.json()
+            const userInfo = results[0]
+            this.firstName = userInfo.name.first
+            this.lastName = userInfo.name.last
+            this.email = userInfo.email
+            this.gender = userInfo.gender
+            this.street = userInfo.location.street.number + " " + userInfo.location.street.name + ", " + userInfo.location.city + ", " + userInfo.location.country
+            this.picture = userInfo.picture.large
+        }
+    }
+})
+
+profile.mount("#profile")
